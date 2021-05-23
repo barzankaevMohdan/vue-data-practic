1dbb873ea1de475abbabb7abc5bf3e85e264db05
Mohdan <mohdan.barzankaev@gmail.com>
2021-05-24 00:20:52 +0300

    create form and firebase

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="container">
-    <div class="card">
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
-    </div>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Create</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    createPerson() {
+      // https://vue-data-284a7-default-rtdb.firebaseio.com/
+      // this.name
+    }
+  }
 }
 </script>
 
